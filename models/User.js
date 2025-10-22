c41fdeafc66e84ffc396fede1bbfa78c1da4dfa5
rodrigojmayer <rodrigojmayer@gmail.com>
2025-10-22 20:32:11 -0300

    fixed unique repeated user phone field

--- models/User.js
+++ models/User.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    phone: { type: String, unique: true },
+    phone: { type: String },
     name: String,
     enabled: { type: Boolean, default: true },
     deleted: { type: Boolean, default: false }
 }, { timestamps: true });
+
+userSchema.index(
+    { phone: 1 },
+    { unique: true, partialFilterExpression: { phone: { $type: "string", $ne: "" } } }
+);
 
 export default mongoose.model("User", userSchema);
