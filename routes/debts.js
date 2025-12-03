fef3c5d867a792060ba3f5efaf0b71c4366be894
rodrigojmayer <rodrigojmayer@gmail.com>
2025-12-03 13:19:32 -0300

    added initial_amount to debts model and route

--- routes/debts.js
+++ routes/debts.js
@@ -135,14 +135,14 @@ router.get("/debtor/:id_user_debtor", async (req, res) => {
 // Update debt
 router.patch("/:id_debt", async (req, res) => {
     const { id_debt } = req.params
-    const { id_user_creditor, id_user_debtor, date_debt, detail, amount, dolar_google, status, date_due, alert_enabled, alerted, currency, enabled, deleted } = req.body
+    const { id_user_creditor, id_user_debtor, date_debt, detail, initial_amount, amount, dolar_google, status, date_due, alert_enabled, alerted, currency, enabled, deleted } = req.body
 
     try {
         // Validate the ID first
         if(!mongoose.Types.ObjectId.isValid(id_debt)) {
             return res.status(400).json({ error: "Invalid debt ID" });
         }
-        const updateData = { id_user_creditor, id_user_debtor, date_debt, detail, amount, dolar_google, status, date_due, alert_enabled, alerted, currency, enabled, deleted }
+        const updateData = { id_user_creditor, id_user_debtor, date_debt, detail, initial_amount, amount, dolar_google, status, date_due, alert_enabled, alerted, currency, enabled, deleted }
 
         // Find the debt by ID
         const debt = await Debt.findOneAndUpdate(
@@ -212,7 +212,7 @@ router.delete("/:id_debt", async (req, res) => {
 
 // create debt by debtor email
 router.post("/create-by-debtor-email", async (req, res) => {
-    let { id_user_creditor, id_user_debtor, email_debtor, detail, amount, currency, date_due } = req.body
+    let { id_user_creditor, id_user_debtor, email_debtor, detail, initial_amount, amount, currency, date_due } = req.body
     try {
         // Find the debtor
         if(!id_user_debtor){
@@ -227,6 +227,7 @@ router.post("/create-by-debtor-email", async (req, res) => {
             id_user_creditor: id_user_creditor,
             id_user_debtor: id_user_debtor,
             detail: detail,
+            initial_amount: initial_amount,
             amount: amount,
             date_due: date_due,
             currency: currency,
