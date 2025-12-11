f6da1655fed35a4fe0c5bf8350c029a9e0f9c0c3
rodrigojmayer <rodrigojmayer@gmail.com>
2025-12-11 20:51:20 -0300

    limit max amount debts and payments

--- models/Payment.js
+++ models/Payment.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
     id_debt: { type: mongoose.Schema.Types.ObjectId, ref: "Debt", required: true },
-    amount: { type: Number, required: true, min: 0 },
-    pending: { type: Number, required: true, min: 0 },
+    amount: { type: Number, required: true, min: 0, max: 1000000000 },
+    pending: { type: Number, required: true, min: 0, max: 1000000000  },
     date_payment: { type: Date, default: Date.now },
     dolar_google: Number,
     enabled: { type: Boolean, default: true },
